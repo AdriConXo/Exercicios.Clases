28f972409bd9f98c8964c972c9f51d8223c9918f
AdriConxo <adri82cx@gmail.com>
2025-02-18 20:12:50 +0100

    Subiendo Exercicios.Clases con ejercicios

---
+++ Exercicio-1.ts
@@ -0,0 +1,16 @@
+//Exercicio 1
+/**
+ * Crea unha clase que conteña as seguintes propiedades:
+ * nome
+ * apelido1
+ * apelido2
+ * email
+ * data de nacemento
+ * Crea Un método o cal utilizarás para sacra os datos introducidos na instancia pola consola
+ */
+
+class DatosPersonais {
+    nome: string;
+    apelido1
+
+}
